7bf7de8aaa9594ce506318ba84eb1823fddb8300
OJP98 <ojuarez_p@hotmail.com>
2020-03-09 23:29:41 -0600

    Correlación de variables numéricas

--- solucion.R
+++ solucion.R
@@ -1,10 +1,31 @@
-setwd("C:/Users/wuser/Documents/UVG/4to Año/Primer Semestre/Minería de datos/HT3/decisionTreeLab")
+# setwd("./")
+library("ggpubr")
 
 data<-read.csv("./Data/train.csv",stringsAsFactors = FALSE)
-
-
-View(data)
 
 #Resumen de datos
 summary(data)
 str(data)
+
+# numerics <- c(2,4,5,18,19,20,21,27,35,37,38,39,44,45,46,47,48,49,50,51,52,53,55,57,60,62,63,67,68,69,70,71,72,76,77,78,81)
+rowNumbers <- c()
+varNames <- c()
+
+# Obtener vector de columnas numéricas
+for(name in colnames(data)){
+  if(is.numeric(data[1,name]) && name != "Id")
+  {
+    rowNumbers <- c(rowNumbers, which(colnames(data)==name))
+    varNames <- c(varNames, name)
+  }
+}
+
+# Obtener correlación de cada columna en el dataframe
+correlation <- cor(data[,rowNumbers],data$SalePrice,method = c("pearson", "kendall", "spearman"))
+
+# Crear y ordenar tabla
+corrTable <- data.frame(rowNumbers, varNames, correlation)
+corrTableDesc <- corrTable[order(-correlation),]
+
+
+
