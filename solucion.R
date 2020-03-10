9dddaca9b0b9b8dbbfc0f9b542322e43bed5dad5
josepablito16 <josepablocif16@gmail.com>
2020-03-09 23:26:42 -0600

    Importación de datos

---
+++ solucion.R
@@ -0,0 +1,10 @@
+setwd("C:/Users/wuser/Documents/UVG/4to Año/Primer Semestre/Minería de datos/HT3/decisionTreeLab")
+
+data<-read.csv("./Data/train.csv",stringsAsFactors = FALSE)
+
+
+View(data)
+
+#Resumen de datos
+summary(data)
+str(data)
